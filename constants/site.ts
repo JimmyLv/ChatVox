bec92f38745cefb6414d06cbcc58916cf3c83eca
JimmyLv <jimmy.jinglv@gmail.com>
2023-04-16 14:51:19 +0800

    feat: add page description

---
+++ constants/site.ts
@@ -0,0 +1,2 @@
+export const CURRENT_DOMAIN = 'chatvox.aitodo.co'
+export const BASE_DOMAIN = `https://${CURRENT_DOMAIN}`
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,29 @@
+import { BASE_DOMAIN, CURRENT_DOMAIN } from '@/constants/site'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+  const t = (key: string) => key
+  let description = t(
+    `Chat With Any Video, challenge myself to complete in @Supabase AI Hackathon.`
+  )
+  let ogimage = `${BASE_DOMAIN}/og-image.png`
+  let sitename = CURRENT_DOMAIN
+  let title = t('ChatVox · Chat With Any Video')
+
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/icon_32x32@2x.png" />
+        <meta name="description" content={description} />
+        <meta property="og:site_name" content={sitename} />
+        <meta property="og:description" content={description} />
+        <meta property="og:title" content={title} />
+        <meta property="og:image" content={ogimage} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={ogimage} />
+      </Head>
       <body>
         <Main />
         <NextScript />
